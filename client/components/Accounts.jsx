6563a58d75ebabd917c46c36007ca46a6dda2c57
Scott Ashmore <sashmore@hi-res.net>
2016-11-24 21:42:11 +0000

    updates

--- client/components/Accounts.jsx
+++ client/components/Accounts.jsx
@@ -8,10 +8,12 @@ export default class AccountsUIWrapper extends Component {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons, findDOMNode(this.refs.container));
   }
+
   componentWillUnmount() {
     // Clean up Blaze view
     Blaze.remove(this.view);
   }
+
   render() {
     // Just render a placeholder container that will be filled in
     return <span ref="container" />;
