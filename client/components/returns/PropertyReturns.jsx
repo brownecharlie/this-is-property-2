87a951788b0ad3d71e3348dd10d215087bd68f58
Scott Ashmore <dev.scottashmore@gmail.com>
2017-03-09 23:23:28 +0000

    optimisations

--- client/components/returns/PropertyReturns.jsx
+++ client/components/returns/PropertyReturns.jsx
@@ -1,11 +1,19 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { connect } from 'react-redux';
 
 import Icon from 'antd/lib/icon';
 import 'antd/lib/icon/style/css';
 
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
 
 import formatCurrency from '../../utils/formatCurrency';
 import {
@@ -24,125 +32,121 @@ import {
   calculateOwnersProfit,
 } from '../../utils/calculatePropertyReturns';
 
-class PropertyReturns extends Component {
-  render() {
-    const {
-      growth,
-      holdPeriod,
-      price,
-      saleAgentFees,
-      saleLegalFees,
-      purchaseAgentFees,
-      purchaseLegalFees,
-      surveyFees,
-      rentalIncome,
-      brokerFee,
-      stampDuty,
-      deposit,
-      loanType,
-      interestRate,
-      term,
-      buyToLet,
-      adminDistrict,
-      fiveYearGrowth,
-      governmentLoan,
-      headerClicked,
-    } = this.props;
+function PropertyReturns({
+  growth,
+  holdPeriod,
+  price,
+  saleAgentFees,
+  saleLegalFees,
+  purchaseAgentFees,
+  purchaseLegalFees,
+  surveyFees,
+  rentalIncome,
+  brokerFee,
+  stampDuty,
+  deposit,
+  loanType,
+  interestRate,
+  term,
+  buyToLet,
+  adminDistrict,
+  fiveYearGrowth,
+  governmentLoan,
+  headerClicked,
+}) {
+  const salePrice = calculateSalePrice(growth, holdPeriod, price);
+  const saleCosts = calculateSaleCosts(saleAgentFees, salePrice, saleLegalFees);
+  const purchaseCosts = calculatePurchaseCosts(purchaseAgentFees, price, purchaseLegalFees, surveyFees, stampDuty, price);
+  const monthlyMortgagePayment = calculateMortgagePayments(price, deposit, loanType, interestRate, term).monthly;
+  const totalMortgagePayment = calculateTotalMortgagePayment(monthlyMortgagePayment, holdPeriod, brokerFee);
+  const totalRentalIncome = calculateTotalRentalIncome(rentalIncome, holdPeriod);
+  const profit = calculateProfit(salePrice, saleCosts, price, purchaseCosts, totalMortgagePayment, brokerFee, totalRentalIncome);
+  const profitOnCost = calculateProfitOnCost(profit, saleCosts, price, purchaseCosts, totalMortgagePayment, totalRentalIncome);
+  const governmentProfit = calculateGovernmentProfit(price, salePrice, governmentLoan);
+  const ownersProfit = calculateOwnersProfit(profit, governmentProfit);
 
-    const salePrice = calculateSalePrice(growth, holdPeriod, price);
-    const saleCosts = calculateSaleCosts(saleAgentFees, salePrice, saleLegalFees);
-    const purchaseCosts = calculatePurchaseCosts(purchaseAgentFees, price, purchaseLegalFees, surveyFees, stampDuty, price);
-    const monthlyMortgagePayment = calculateMortgagePayments(price, deposit, loanType, interestRate, term).monthly;
-    const totalMortgagePayment = calculateTotalMortgagePayment(monthlyMortgagePayment, holdPeriod, brokerFee);
-    const totalRentalIncome = calculateTotalRentalIncome(rentalIncome, holdPeriod);
-    const profit = calculateProfit(salePrice, saleCosts, price, purchaseCosts, totalMortgagePayment, brokerFee, totalRentalIncome);
-    const profitOnCost = calculateProfitOnCost(profit, saleCosts, price, purchaseCosts, totalMortgagePayment, totalRentalIncome);
-    const governmentProfit = calculateGovernmentProfit(price, salePrice, governmentLoan);
-    const ownersProfit = calculateOwnersProfit(profit, governmentProfit);
+  const data = [
+    { name: 'Assumed Growth', value: growth, color: '#0088FE' },
+    { name: 'Projected Growth', value: fiveYearGrowth, color: '#00C49F' },
+  ];
 
-    const data = [
-      { name: 'Assumed Growth', value: growth, color: '#0088FE' },
-      { name: 'Projected Growth', value: fiveYearGrowth, color: '#00C49F' },
-    ];
-
-    return (
-      <section className="ReturnsContainer-section is-active">
-        <header className="ReturnsContainer-header" onClick={headerClicked}>
-          <h3>Property Returns</h3>
-          <Icon type="caret-down" className="u-floatRight"/>
-        </header>
-        <div className="ReturnsContainer-values">
-          <ul>
-            <li className="ReturnsContainer-profit">
-              <span>Profit on cost: </span>
-              <span className="u-floatRight">{profitOnCost.toFixed(2)}%</span>
-            </li>
-            <li className="ReturnsContainer-profit">
-              <span>Profit: </span>
-              <span className="u-floatRight">{formatCurrency(profit)}</span>
-            </li>
-            <li className="ReturnsContainer-salePrice">
-              <span>Sale price: </span>
-              <span className="u-floatRight">{formatCurrency(salePrice)}</span>
-            </li>
-            <li className="ReturnsContainer-saleCosts">
-              <span>Sale costs: </span>
-              <span className="u-floatRight">{formatCurrency(saleCosts)}</span>
-            </li>
-            <li className="ReturnsContainer-price">
-              <span>Purchase price: </span>
-              <span className="u-floatRight">{formatCurrency(price)}</span>
-            </li>
-            <li className="ReturnsContainer-costs">
-              <span>Purchase costs: </span>
-              <span className="u-floatRight">{formatCurrency(purchaseCosts)}</span>
-            </li>
-            <li className="ReturnsContainer-mortgagePayments">
-              <span>Mortgage payments: </span>
-              <span className="u-floatRight">{formatCurrency(totalMortgagePayment)}</span>
-            </li>
-            <li className="ReturnsContainer-rentalIncome">
-              <span>Rental income: </span>
-              <span className="u-floatRight">{formatCurrency(totalRentalIncome)}</span>
-            </li>
-            <li className="ReturnsContainer-holdPeriod">
-              <span>Hold period (years): </span>
-              <span className="u-floatRight">{`${holdPeriod} years`}</span>
-            </li>
-            <li className="ReturnsContainer-holdPeriod">
-              <span>Growth (% p.a.): </span>
-              <span className="u-floatRight">{`${growth}%`}</span>
-            </li>
-            <li className="ReturnsContainer-buyToLet">
-              <span>Buy to let: </span>
-              <span className="u-floatRight">{buyToLet ? 'Yes' : 'No'}</span>
-            </li>
-            <li className="ReturnsContainer-stamDuty">
-              <span>Stamp duty: </span>
-              <span className="u-floatRight">{`${stampDuty}%`}</span>
-            </li>
-            {fiveYearGrowth ?
-            <li className="ReturnsContainer-chart">
-              <span>5 year average growth in {adminDistrict}</span>
-              <ResponsiveContainer height={230}>
-                <BarChart width={300} height={230} data={data}>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <CartesianGrid strokeDasharray="5 5" />
-                  <Bar dataKey="value" isAnimationActive={false}>
-                  {
-                    data.map((entry, index) => <Cell key={index} fill={entry.color}/>)
-                  }
-                  </Bar>
-                </BarChart>
-              </ResponsiveContainer>
-            </li> : null
-            }
-          </ul>
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className="ReturnsContainer-section is-active">
+      <header className="ReturnsContainer-header" onClick={headerClicked}>
+        <h3>Property Returns</h3>
+        <Icon type="caret-down" className="u-floatRight"/>
+      </header>
+      <div className="ReturnsContainer-values">
+        <ul>
+          <li className="ReturnsContainer-profit">
+            <span>Profit on cost: </span>
+            <span className="u-floatRight">{profitOnCost.toFixed(2)}%</span>
+          </li>
+          <li className="ReturnsContainer-profit">
+            <span>Profit: </span>
+            <span className="u-floatRight">{formatCurrency(profit)}</span>
+          </li>
+          <li className="ReturnsContainer-salePrice">
+            <span>Sale price: </span>
+            <span className="u-floatRight">{formatCurrency(salePrice)}</span>
+          </li>
+          <li className="ReturnsContainer-saleCosts">
+            <span>Sale costs: </span>
+            <span className="u-floatRight">{formatCurrency(saleCosts)}</span>
+          </li>
+          <li className="ReturnsContainer-price">
+            <span>Purchase price: </span>
+            <span className="u-floatRight">{formatCurrency(price)}</span>
+          </li>
+          <li className="ReturnsContainer-costs">
+            <span>Purchase costs: </span>
+            <span className="u-floatRight">{formatCurrency(purchaseCosts)}</span>
+          </li>
+          <li className="ReturnsContainer-mortgagePayments">
+            <span>Mortgage payments: </span>
+            <span className="u-floatRight">{formatCurrency(totalMortgagePayment)}</span>
+          </li>
+          <li className="ReturnsContainer-rentalIncome">
+            <span>Rental income: </span>
+            <span className="u-floatRight">{formatCurrency(totalRentalIncome)}</span>
+          </li>
+          <li className="ReturnsContainer-holdPeriod">
+            <span>Hold period (years): </span>
+            <span className="u-floatRight">{`${holdPeriod} years`}</span>
+          </li>
+          <li className="ReturnsContainer-holdPeriod">
+            <span>Growth (% p.a.): </span>
+            <span className="u-floatRight">{`${growth}%`}</span>
+          </li>
+          <li className="ReturnsContainer-buyToLet">
+            <span>Buy to let: </span>
+            <span className="u-floatRight">{buyToLet ? 'Yes' : 'No'}</span>
+          </li>
+          <li className="ReturnsContainer-stamDuty">
+            <span>Stamp duty: </span>
+            <span className="u-floatRight">{`${stampDuty}%`}</span>
+          </li>
+          {fiveYearGrowth ?
+          <li className="ReturnsContainer-chart">
+            <span>5 year average growth in {adminDistrict}</span>
+            <ResponsiveContainer height={230}>
+              <BarChart width={300} height={230} data={data}>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="5 5" />
+                <Bar dataKey="value" isAnimationActive={false}>
+                {
+                  data.map((entry, index) => <Cell key={index} fill={entry.color}/>)
+                }
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </li> : null
+          }
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 PropertyReturns.propTypes = {
